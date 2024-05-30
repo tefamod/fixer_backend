dd150064df0e8c74dfb178def34d3fdcccbfaaa8
tefamod22@gmail.com <you@example.com>
2024-05-30 23:32:19 +0300

    repair details

--- routes/repairingRoute.js
+++ routes/repairingRoute.js
@@ -8,11 +8,13 @@ const {
   getAllComRepairs,
   getCarRepairsByid,
   getCarRepairsByGenCode,
+  getRepairsReport,
 } = require("../services/repairingService");
-
 router.route("/").post(createRepairing).get(getAllComRepairs);
 router.route("/:carNumber").get(getCarRepairsByNumber);
 router.route("/getById/:id").get(getCarRepairsByid);
 router.route("/gen/:generatedCode").get(getCarRepairsByGenCode);
+router.route("/report/:id").get(getRepairsReport);
 router.route("/:serviceId").put(updateServiceStateById);
+
 module.exports = router;
